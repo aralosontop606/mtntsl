5e73995b55a01f2a885013e72ae7c8c6c24d6d6c
aralosontop606 <aralosontop3@gmail.com>
2026-07-22 13:26:15 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,278 @@
+const SITE_CONFIG = {
+  name: "MTNTSL",
+
+  backgroundImage: "https://file.garden/alTaNaMGUV82xki8/mtntsl/banner.png",
+
+  favicon: "https://cdn.discordapp.com/attachments/1507628962447364227/1529298282059268239/MTNTXSL.png?ex=6a616d83&is=6a601c03&hm=f3463c6ab5c64b733f4ed6e6a382513bcf35c1599ec84f5267d969ce9867ee9c",
+
+
+  discordInvite: "https://discord.gg/kV8WaGJV4W",
+
+  discordPreview: {
+    name: "MTNTSL",    
+    icon: "https://cdn.discordapp.com/attachments/1507628962447364227/1529298282059268239/MTNTXSL.png?ex=6a616d83&is=6a601c03&hm=f3463c6ab5c64b733f4ed6e6a382513bcf35c1599ec84f5267d969ce9867ee9c",                 
+    online: "1",         
+    members: "1"                  
+  },
+
+
+  music: {
+    enabled: true,
+    title: "TRACK NAME",
+    src: "https://file.garden/alTaNaMGUV82xki8/mtntsl/Uncle%20Dags%20-%20ORIGIMAL%20(feat.%20RJD).mp3"
+  }
+};
+
+const ROSTER = {
+  founders: [
+    {
+      identity: {
+        name: "KAII", tag: "FOUNDER", bio: "kumain kaba ng tae",
+        badges: [
+            "OG PLAYER",
+            "GAMER"
+        ]
+      },
+      appearance: {
+        avatar: "https://file.garden/alTaNaMGUV82xki8/Kaii/avatar.png",
+        decoration: "https://file.garden/alTaNaMGUV82xki8/Kaii/decoration.gif",
+        status: "online",
+        statusMessage: "Playing Valorant",
+        media: "https://file.garden/alTaNaMGUV82xki8/Kaii/banner.png"
+      },
+      favorites: {
+        song: "Nothin' on You (feat. Bruno Mars)", songUrl: "https://open.spotify.com/track/18zrqdHev7Niaz2MK6P0Rp",
+        game: "VALORANT", gameUrl: "https://playvalorant.com/en-gb/"
+      },
+      links: {
+        website: "http://mtntsl.xyz/kaii",
+        profile: "https://discord.com/users/1494726733289623663"
+      },
+      socials: {
+        tiktok: "https://www.tiktok.com/@yrkaiii_", instagram: "https://www.instagram.com/yrkaii_/", discord: "", spotify: "https://open.spotify.com/user/313zx3uqnmvfjzbqtfqounzvzine", youtube: "",
+        facebook: "", twitter: "", github: "", roblox: "https://www.roblox.com/users/3193525342/profile", twitch: "",
+        kick: "", steam: "", valorant: "https://tracker.gg/valorant/profile/riot/MTNTxKaii%23Clai", mediafire: ""
+      }
+    },
+    {
+      identity: {
+        name: "PRIMO", tag: "FOUNDER", bio: ".",
+        badges: [
+            "EXPLOITER",
+            "OG PLAYER",
+            "DEVELOPER"
+        ]
+      },
+      appearance: {
+        avatar: "https://file.garden/alTaNaMGUV82xki8/primo/avatar.png",
+        decoration: "https://file.garden/alTaNaMGUV82xki8/primo/decoration.gif",
+        status: "offline",
+        statusMessage: "Playing Roblox",
+        media: "https://file.garden/alTaNaMGUV82xki8/primo/banner.png"
+      },
+      favorites: {
+        song: "Ravyn Lenae - Love Me Not", songUrl: "https://open.spotify.com/track/3ld1FzyNgpZwGn4h9cOXrq",
+        game: "ROBLOX", gameUrl: "https://www.roblox.com/"
+      },
+      links: {
+        website: "http://mtntsl.xyz/primo",
+        profile: "https://discord.com/users/1337086999898755172"
+      },
+      socials: {
+        tiktok: "https://www.tiktok.com/@primo_mapagmahal", instagram: "", discord: "", spotify: "", youtube: "",
+        facebook: "", twitter: "", github: "", roblox: "https://www.roblox.com/users/8000388209/profile", twitch: "",
+        kick: "", steam: "", valorant: "", mediafire: ""
+      }
+    },
+    {
+      identity: { name: "ZIX", tag: "FOUNDER", bio: "susubo kaba?", 
+      badges: [
+        "GAMER",
+        "CERTIFIED KUPAL",
+        "KUSHIECAKES"
+      ]
+     },
+      appearance: {
+        avatar: "https://file.garden/alTaNaMGUV82xki8/zix/avatar.png",
+        decoration: "https://file.garden/alTaNaMGUV82xki8/zix/decoration.gif",
+        status: "idle",
+        statusMessage: "",
+        media: ""
+      },
+      favorites: {
+        song: "", songUrl: "",
+        game: "", gameUrl: ""
+      },
+      links: {
+        website: "http://mtntsl.xyz/zix",
+        profile: "https://discord.com/users/1137359956572635226"
+      },
+      socials: {
+        tiktok: "https://www.tiktok.com/@xizixzixix_", instagram: "", discord: "", spotify: "https://open.spotify.com/user/31k5d5zxjlarspnzqzq4qefkgk7m", youtube: "",
+        facebook: "", twitter: "", github: "", roblox: "https://www.roblox.com/users/3529475501/profile", twitch: "",
+        kick: "", steam: "", valorant: "", mediafire: ""
+      }
+    }
+  ],
+
+  crew: [
+    {
+      identity: { name: "JOSH", tag: "ORIGIN", bio: "", 
+      badges: [
+        "GAMER",
+        "NO SLEEP",
+        "DAY ONE"
+      ]
+     },
+      appearance: {
+        avatar: "https://file.garden/alTaNaMGUV82xki8/josh/avatar.png",
+        decoration: "https://file.garden/alTaNaMGUV82xki8/josh/decoration.gif",
+        status: "online",
+        statusMessage: "",
+        media: "https://file.garden/alTaNaMGUV82xki8/josh/banner.mp4"
+      },
+      favorites: {
+        song: "Billie Eilish - when the party's over", songUrl: "https://open.spotify.com/track/43zdsphuZLzwA9k4DJhU0I",
+        game: "", gameUrl: ""
+      },
+      links: { website: "http://mtntsl.xyz/josh", profile: "https://discord.com/users/1502800190262870128" },
+      socials: {
+        tiktok: "https://www.tiktok.com/@joshnasisaac", instagram: "", discord: "", spotify: "https://open.spotify.com/user/31s7vuw44pufzi7t3mu4xdcdrzha", youtube: "",
+        facebook: "", twitter: "", github: "", roblox: "https://www.roblox.com/users/10877272029/profile", twitch: "",
+        kick: "", steam: "", valorant: "", mediafire: ""
+      }
+    },
+    {
+      identity: { name: "YUMS", tag: "ORIGIN", bio: "", 
+      badges: [
+        "DANCER",
+        "SLAY",
+        "STUDY FIRST"
+      ]
+     },
+      appearance: {
+        avatar: "https://file.garden/alTaNaMGUV82xki8/yums/avatar.png",
+        decoration: "https://file.garden/alTaNaMGUV82xki8/yums/decoration.gif",
+        status: "online",
+        statusMessage: "",
+        media: "https://file.garden/alTaNaMGUV82xki8/yums/banner.mp4"
+      },
+      favorites: {
+        song: "", songUrl: "",
+        game: "", gameUrl: ""
+      },
+      links: {
+        website: "http://mtntsl.xyz/yums",
+        profile: "https://discord.com/users/1489805640091045939"
+      },
+      socials: {
+        tiktok: "", instagram: "https://www.instagram.com/m_cyumz", discord: "", spotify: "", youtube: "",
+        facebook: "", twitter: "", github: "", roblox: "https://www.roblox.com/users/10558837802/profile", twitch: "",
+        kick: "", steam: "", valorant: "", mediafire: ""
+      }
+    },
+    {
+      identity: { name: "SCOOPY", tag: "ORIGIN", bio: "WHATS UP NIGGA", badges: [] },
+      appearance: {
+        avatar: "https://file.garden/alTaNaMGUV82xki8/scoopy/avatar.png",
+        decoration: "https://file.garden/alTaNaMGUV82xki8/scoopy/decoration.gif",
+        status: "idle",
+        statusMessage: "",
+        media: "https://file.garden/alTaNaMGUV82xki8/scoopy/banner.mp4"
+      },
+      favorites: {
+        song: "", songUrl: "",
+        game: "", gameUrl: ""
+      },
+      links: { website: "https://mtntsl.xyz/scoopyx", profile: "https://discord.com/users/1137359956572635226" },
+      socials: {
+        tiktok: "", instagram: "", discord: "", spotify: "", youtube: "",
+        facebook: "", twitter: "", github: "", roblox: "https://www.roblox.com/users/4593905285/profile", twitch: "",
+        kick: "", steam: "", valorant: "", mediafire: ""
+      }
+    },
+    {
+      identity: { name: "UNKNOWN", tag: "ORIGIN", bio: "", badges: [] },
+      appearance: {
+        avatar: "https://file.garden/alTaNaMGUV82xki8/unknown/avatar.png",
+        decoration: "",
+        status: "online",
+        statusMessage: "",
+        media: ""
+      },
+      favorites: {
+        song: "JAO - TORPE", songUrl: "https://open.spotify.com/track/3IgRFhLTRlMhYApdcQ8Bot",
+        game: "VALORANT", gameUrl: "https://playvalorant.com/en-gb/"
+      },
+      links: { website: "", profile: "https://discord.com/users/1343234301390487592" },
+      socials: {
+        tiktok: "", instagram: "", discord: "", spotify: "", youtube: "",
+        facebook: "", twitter: "", github: "", roblox: "", twitch: "",
+        kick: "", steam: "", valorant: "", mediafire: ""
+      }
+    }
+  ],
+
+  staff: [
+    {
+      identity: { name: "MICS", tag: "MEMBER", bio: "", badges: [] },
+      appearance: {
+        avatar: "https://cdn.discordapp.com/embed/avatars/3.png",
+        decoration: "",
+        status: "dnd",
+        statusMessage: "",
+        media: ""
+      },
+      favorites: {
+        song: "", songUrl: "",
+        game: "", gameUrl: ""
+      },
+      links: { website: "", profile: "https://discord.com/users/1500620957562179707" },
+      socials: {
+        tiktok: "", instagram: "", discord: "", spotify: "", youtube: "",
+        facebook: "", twitter: "", github: "", roblox: "https://www.roblox.com/users/8504663492/profile", twitch: "",
+        kick: "", steam: "", valorant: "", mediafire: ""
+      }
+    },
+    {
+      identity: { name: "XIE", tag: "MEMBER", bio: "", badges: [] },
+      appearance: {
+        avatar: "https://cdn.discordapp.com/avatars/1437809698828193873/657e612cb0a725ab28018ffb42efe652.png?size=256",
+        decoration: "",
+        status: "online",
+        statusMessage: "",
+        media: ""
+      },
+      favorites: {
+        song: "", songUrl: "",
+        game: "", gameUrl: ""
+      },
+      links: { website: "", profile: "https://discord.com/users/1005308937123401789" },
+      socials: {
+        tiktok: "", instagram: "", discord: "", spotify: "", youtube: "",
+        facebook: "", twitter: "", github: "", roblox: "https://www.roblox.com/users/2591041614/profile", twitch: "",
+        kick: "", steam: "", valorant: "", mediafire: ""
+      }
+    },
+    {
+      identity: { name: "CLAI", tag: "MEMBER", bio: "", badges: [] },
+      appearance: {
+        avatar: "https://cdn.discordapp.com/attachments/1500976047749988593/1529303280662872104/IMG_20260627_164556_988.png?ex=6a61722b&is=6a6020ab&hm=07eda679ea3d0de1e424f50686f1ce3d6b6e82b30da1abe1cd0d64752e15e067",
+        decoration: "",
+        status: "idle",
+        statusMessage: "",
+        media: ""
+      },
+      favorites: {
+        song: "", songUrl: "",
+        game: "ROBLOX", gameUrl: "http://roblox.com/"
+      },
+      links: { website: "", profile: "https://discord.com/users/1403554543345860722" },
+      socials: {
+        tiktok: "", instagram: "", discord: "", spotify: "", youtube: "",
+        facebook: "", twitter: "", github: "", roblox: "", twitch: "",
+        kick: "", steam: "", valorant: "", mediafire: ""
+      }
+    }
+  ]
+};
